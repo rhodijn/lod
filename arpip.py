a92fb54bce4c9024c86b9e523d2124fa0e52bcb4
Rhodijn <roger.zollinger@proton.me>
2025-07-20 15:58:11 +0200

    update python representation

--- arpip.py
+++ arpip.py
@@ -16,6 +16,7 @@ from rdflib.namespace import RDF
 
 g = rdflib.Graph()
 nm = g.namespace_manager
+triples = []
 
 # @prefix bf:
 ns_bf = rdflib.Namespace('http://id.loc.gov/ontologies/bibframe/')
@@ -25,18 +26,34 @@ nm.bind('bf', ns_bf)
 ns_bibo = rdflib.Namespace('http://purl.org/ontology/bibo/')
 nm.bind('bibo', ns_bibo)
 
+# @prefix:dct
+ns_dct = rdflib.Namespace('http://purl.org/dc/terms/')
+nm.bind('dct', ns_dct)
+
+# @prefix:ex
+ns_ex = rdflib.Namespace('http://example.org/')
+nm.bind('ex', ns_ex)
+
 # @prefix:fabio:
 ns_fabio = rdflib.Namespace('http://purl.org/spar/fabio/')
 nm.bind('fabio', ns_fabio)
 
-uri = 'https://datadryad.org/dataset/doi:10.5061/dryad.wstqjq2nj'
-s = rdflib.URIRef(uri)
+s = rdflib.URIRef('https://datadryad.org/dataset/doi:10.5061/dryad.wstqjq2nj')
+p = ns_dct.creator
 
+g.add((s, p, ns_ex.ManuelGil))
+g.add((s, p, ns_ex.MassimoMaiolo))
+g.add((s, p, ns_ex.GholamhosseinJowkar))
+g.add((s, p, ns_ex.JūlijaPečerska))
+g.add((s, p, ns_ex.MariaAnisimova))
+
+g.add((s, RDF.type, ns_fabio.Dataset))
+
+s = rdflib.URIRef('https://doi.org/10.1093/sysbio/syac050')
 p = RDF.type
 
-o = ns_fabio.Dataset
-
-g.add((s, p, o))
+g.add((s, p, ns_dct.BibliographicResource))
+g.add((s, p, ns_bibo.Article))
 
 for s, p, o in g:
     print(s, p, o)

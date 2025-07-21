c9b4bb9644b345dc353cb353575a93e75aeb020a
Rhodijn <roger.zollinger@proton.me>
2025-07-21 20:14:21 +0200

    almost done with second paragraph

--- arpip.py
+++ arpip.py
@@ -62,7 +62,6 @@ nm.bind('xsd', ns_xsd)
 #dataset
 s = rdflib.URIRef('https://datadryad.org/dataset/doi:10.5061/dryad.wstqjq2nj')
 p = ns_dct.creator
-
 g.add((s, p, ns_ex.ManuelGil))
 g.add((s, p, ns_ex.MassimoMaiolo))
 g.add((s, p, ns_ex.GholamhosseinJowkar))
@@ -70,25 +69,64 @@ g.add((s, p, ns_ex.JūlijaPečerska))
 g.add((s, p, ns_ex.MariaAnisimova))
 
 o = rdflib.Literal('ARPIP: Ancestral sequence Reconstruction with insertions and deletions under the Poisson Indel Process', datatype=rdflib.XSD.string)
-
 g.add((s, ns_dct.title, o))
 
 g.add((s, ns_fabio.hasPublicationYear, rdflib.Literal(2022, datatype=rdflib.XSD.integer)))
 
-p = ns_dct.modified
-
-g.add((s, p, rdflib.Literal('2022-07-26', datatype=rdflib.XSD.date)))
+g.add((s, ns_dct.modified, rdflib.Literal('2022-07-26', datatype=rdflib.XSD.date)))
 
 p = ns_dct.relation
-
 g.add((s, p, rdflib.URIRef('https://doi.org/10.1093/sysbio/syac050')))
 g.add((s, p, rdflib.URIRef('https://doi.org/10.5281/zenodo.7078704')))
 
 g.add((s, rdflib.RDF.type, ns_fabio.Dataset))
+g.add((s, ns_bibo.numPages, rdflib.Literal(12, datatype=rdflib.XSD.integer)))
+g.add((s, ns_bibo.pageStart, rdflib.Literal(307, datatype=rdflib.XSD.integer)))
+g.add((s, ns_bibo.pageEnd, rdflib.Literal(318, datatype=rdflib.XSD.integer)))
+g.add((s, ns_bibo.pages, rdflib.Literal('307-318', datatype=rdflib.XSD.string)))
+
+p = ns_dct.subject
+g.add((s, p, rdflib.Literal('computer and information sciences', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('ancestral sequences', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('coronaviridae', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('dynamic programming', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('evolutionary stochastic process', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('indel', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('joint ancestral sequence reconstruction', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('maximum likelihood ancestral reconstruction', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('Poisson indel Process', datatype=rdflib.XSD.string)))
+g.add((s, p, rdflib.Literal('SARS-CoV', datatype=rdflib.XSD.string)))
+
+o = rdflib.Literal('Modern phylogenetic methods allow inference of ancestral molecular sequences given an alignment and phylogeny relating present day sequences. This provides insight into the evolutionary history of molecules, helping to understand gene function and to study biological processes such as adaptation and convergent evolution across a variety of applications. Here we propose a dynamic programming algorithm for fast joint likelihood-based reconstruction of ancestral sequences under the Poisson Indel Process (PIP). Unlike previous approaches, our method, named ARPIP, enables the reconstruction with insertions and deletions based on an explicit indel model. Consequently, inferred indel events have an explicit biological interpretation. Likelihood computation is achieved in linear time with respect to the number of sequences. Our method consists of two steps, namely finding the most probable indel points and reconstructing ancestral sequences. First, we find the most likely indel points and prune the phylogeny to reflect the insertion and deletion events per site. Second, we infer the ancestral states on the pruned subtree in a manner similar to FastML. We applied ARPIP on simulated datasets and on real data from the Betacoronavirus genus. ARPIP reconstructs both the indel events and substitutions with a high degree of accuracy. Our method fares well when compared to established state-of-the-art methods such as FastML and PAML. Moreover, the method can be extended to explore both optimal and suboptimal reconstructions, include rate heterogeneity through time and more. We believe it will expand the range of novel applications of ancestral sequence reconstruction.', datatype=rdflib.XSD.string)
+g.add((s, ns_dct.abstract, o))
+
+o = rdflib.Literal('Jowkar, Gholamhossein; Pečerska, Jūlija; Maiolo, Massimo et al. (2022). ARPIP: Ancestral sequence Reconstruction with insertions and deletions under the Poisson Indel Process [Dataset]. Dryad. https://doi.org/10.5061/dryad.wstqjq2nj', datatype=rdflib.XSD.string)
+g.add((s, ns_dct.bibliographicCitation, o))
+
+g.add((s, ns_dct.license, rdflib.URIRef('https://creativecommons.org/publicdomain/zero/1.0/')))
+g.add((s, ns_dct.license, rdflib.Literal('Public Domain', datatype=rdflib.XSD.string)))
+
+g.add((s, ns_dct.hasPart, rdflib.URIRef('https://datadryad.org/downloads/file_stream/1688696')))
+g.add((s, ns_dct.hasPart, rdflib.URIRef('https://datadryad.org/downloads/file_stream/1688697')))
+
+g.add((s, ns_ex.fundedBy, rdflib.URIRef('http://viaf.org/viaf/150746866')))
 
 # article
 s = rdflib.URIRef('https://doi.org/10.1093/sysbio/syac050')
+p = ns_dct.creator
+g.add((s, p, ns_ex.ManuelGil))
+g.add((s, p, ns_ex.MassimoMaiolo))
+g.add((s, p, ns_ex.GholamhosseinJowkar))
+g.add((s, p, ns_ex.JūlijaPečerska))
+g.add((s, p, ns_ex.MariaAnisimova))
 
+g.add((s, ns_bibo.issn, rdflib.Literal('1063-5157', datatype=rdflib.XSD.string)))
+
+g.add((s, ns_bibo.volume, rdflib.Literal(72, datatype=rdflib.XSD.integer)))
+
+g.add((s, ns_dc.title, rdflib.Literal('ARPIP: Ancestral Sequence Reconstruction with Insertions and Deletions under the Poisson Indel Process', datatype=rdflib.XSD.string)))
+
+g.add((s, ns_dct.issued, rdflib.Literal('2022-03', datatype=rdflib.XSD.gYearMonth)))
 
 g.add((s, rdflib.RDF.type, ns_dct.BibliographicResource))
 g.add((s, rdflib.RDF.type, ns_bibo.Article))
